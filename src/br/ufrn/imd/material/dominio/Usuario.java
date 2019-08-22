681a0998f48ed7d1d8a12fbedc590579853f9ca1
protonsnet <protonsnet@bol.com.br>
2019-08-22 14:23:57 -0300

    Material JSF

---
+++ src/br/ufrn/imd/material/dominio/Usuario.java
@@ -0,0 +1,65 @@
+package br.ufrn.imd.material.dominio;
+
+public class Usuario {
+	
+	private int id;
+	private String login;
+	private String senha;
+	private String nome;
+	
+	public Usuario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		return true;
+	}
+
+}
